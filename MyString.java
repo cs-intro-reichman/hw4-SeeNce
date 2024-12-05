04d9ef37486429e7a1c54c16532dcf8f58529e54
Mor Cohen <mor.cohen02@post.runi.ac.il>
2024-12-05 12:30:31 +0200

    MyStringAdded

--- MyString.java
+++ MyString.java
@@ -18,13 +18,38 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+     String newStr = "";
+     for(int i=0; i<str.length(); i++)
+     {
+        if(str.charAt(i)<='Z' && str.charAt(i)>='A')
+            newStr += (char)(str.charAt(i)+32);
+            else newStr += str.charAt(i);
+     }
+        return newStr;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        if (str2.isEmpty()) {
+            return true;
+        }
+        if(str1.length()<str2.length())
+            return false;
+        int c=0;
+
+        for(int i=0; i<str1.length(); i++){
+            int cs=0;
+            for(int j=0;j<str2.length(); j++)
+            {
+                if ((j + c) >= str1.length() || str1.charAt(j + c) != str2.charAt(j))
+                    break;
+
+                cs++;
+                if(cs==str2.length())
+                    return true;
+            }
+            c++;
+        }
         return false;
     }
 }
